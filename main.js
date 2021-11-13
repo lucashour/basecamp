15712e929c03cd93ab9b94b2bcb980e605dd10fb
Lucas Hourquebie <lucashour1993@gmail.com>
2021-11-13 16:29:45 -0300

    Add tray support to run app in background

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 const path = require("path");
 const fs = require("fs");
-
 const {
   app,
   clipboard,
   Menu,
   MenuItem,
   shell,
+  Tray,
 } = require("electron");
 
 const contextualMenu = new Menu();
 
+let isQuiting;
+let tray;
+
 app.allowRendererProcessReuse = true;
 
-app.on("ready", () => {
+const setupWindow = (app) => {
   const window = require("./src/window");
-  mainWindow = window.createBrowserWindow(app);
+  const mainWindow = window.createBrowserWindow(app);
 
   mainWindow.loadURL("https://basecamp.com");
 
@@ -30,12 +33,47 @@ app.on("ready", () => {
     shell.openExternal(url);
   });
 
+  mainWindow.on('close',(event) => {
+    if (!isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+      event.returnValue = false;
+    }
+  });
+
   contextualMenu.append(new MenuItem({
     label: 'Copy current URL to clipboard',
     click: () => {
       clipboard.writeText(mainWindow.webContents.getURL());
     },
   }));
+
+  return mainWindow;
+};
+
+const setupTray = (app, mainWindow) => {
+  tray = new Tray(path.join(__dirname, '/assets/icons/tray.png'));
+
+  tray.setContextMenu(Menu.buildFromTemplate([
+    {
+      label: 'Open', click: () => {
+        debugger;
+        mainWindow.show();
+      }
+    },
+    {
+      label: 'Quit', click: () => {
+        isQuiting = true;
+        app.quit();
+      }
+    },
+  ]));
+};
+
+app.on("ready", () => {
+  const mainWindow = setupWindow(app);
+
+  setupTray(app, mainWindow);
 });
 
 app.on("web-contents-created", (...[event, webContents]) => {
@@ -45,6 +83,7 @@ app.on("web-contents-created", (...[event, webContents]) => {
   }, false);
 });
 
-app.on("window-all-closed", () => {
-  app.quit();
+app.on("before-quit", () => {
+  isQuiting = true;
 });
+
